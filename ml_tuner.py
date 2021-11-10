39d5b7ddebf272bd48f88110ce21bde2e17a7503
Dominik Schlösser <dsc@dosc.net>
2021-11-10 11:29:20 +0100

    api cleanup

--- ml_tuner.py
+++ ml_tuner.py
@@ -4,10 +4,8 @@ import json
 from ml_env_tools import MLEnv
 
 class MLTuner():
-    def __init__(self, ml_env:MLEnv, model_variant, params):
-        self.params_file = os.path.join(ml_env.root_path, f"params_{model_variant}.json")
+    def __init__(self, ml_env:MLEnv, model_variant):
         self.search_space_file = os.path.join(ml_env.root_path, f"params_search_space_{model_variant}.json")
-        self.params = params
         is_new = True
         if os.path.exists(self.search_space_file) is True:
             with open(self.search_space_file,'r') as f:
@@ -15,22 +13,17 @@ class MLTuner():
                 print(f"Initialized search_space from {self.search_space_file}")
                 is_new = False
         if is_new is True:
-            self.search_space["best_params"] = copy.deepcopy(params)
             self.search_space["best_ev"] = 0
             self.search_space["is_first"] = True
 
-    def tune(self, eval_func):
-        for key in self.search_space["params"]:
-            if key not in self.params:
-                print(f"search_space searches for {key}, but it's not in params!")
-                return self.search_space["best_params"]
-            else: # initialize with first entry in search space
+    def tune(self, param_space, eval_func):
+        for key in param_space:
+            if key not in self.search_space["best_params"]:
                 self.search_space["best_params"][key]=self.search_space["params"][key][0]
-
         p_cnt=0
-        for key in self.search_space["params"]:
+        for key in param_space:
             params=copy.deepcopy(self.search_space["best_params"])
-            vals=self.search_space["params"][key]
+            vals=param_space[key]
             for val in vals:
                 if self.search_space["is_first"] is False:
                     if val==self.search_space["best_params"][key]:
@@ -54,8 +47,6 @@ class MLTuner():
                     print("*********************************************************")
                     print(f"Best parameter set with ev={ev}: {params}")
                     print("*********************************************************")
-                    with open(self.params_file, "w") as f:
-                        json.dump(self.search_space["best_params"], f, indent=4)
                     with open(self.search_space_file, "w") as f:
                         json.dump(self.search_space, f, indent=4)
                 if interrupted>0:
