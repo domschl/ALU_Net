93e7f5f325b26ef0e4ae8f0c3399fb0fb81e876e
Dominik Schlösser <dsc@dosc.net>
2021-12-02 10:36:49 +0100

    support serialization

--- keras_custom_layers.py
+++ keras_custom_layers.py
@@ -5,13 +5,21 @@ from tensorflow.keras import layers
 
 class ResidualBlock(layers.Layer):
     def __init__(self, units):
+        self.units=units
         super(ResidualBlock, self).__init__()
-        self.dense1 = layers.Dense(units)
+        self.dense1 = layers.Dense(self.units)
         self.bn1 = layers.BatchNormalization()
         self.relu = layers.ReLU()
-        self.dense2 = layers.Dense(units)
+        self.dense2 = layers.Dense(self.units)
         self.bn2 = layers.BatchNormalization()
         self.relu2 = layers.ReLU()
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            'units': self.units
+        })
+        return config
 
     def call(self, inputs):
         x=self.dense1(inputs)
